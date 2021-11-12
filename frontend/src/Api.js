ac4ef31b65c8cd5f6608ef197dd3d473816e28bc
tanboonji <tanboonji@hotmail.com>
2021-11-12 15:31:14 +0800

    Fix OTOT Task B3

---
+++ frontend/src/Api.js
@@ -0,0 +1,55 @@
+import axios from 'axios';
+
+const API_URL = 'https://tanboonji-cs3219-otot-taskb.herokuapp.com';
+
+export async function getAllUsers() {
+    try {
+        const res = await axios.get(API_URL + '/getAllCustomer/');
+        return res;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+export async function geCustomer(id) {
+    try {
+        const res = await axios.get(API_URL + '/getCustomer/' + id);
+        return res;
+    } catch (err) {
+        console.log(err);
+        throw err;
+    }
+}
+
+export async function createCustomer(id, email, name) {
+    try {
+        const res = await axios.post(API_URL + '/createCustomer/', {
+            "id": id,
+            "email": email,
+            "name": name
+        });
+        return res;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+export async function updateCustomer(id, email, name) {
+    try {
+        const res = await axios.put(API_URL + '/updateCustomer/' + id, {
+            "email": email,
+            "name": name
+        });
+        return res;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+export async function deleteCustomer(id) {
+    try {
+        return await axios.delete(API_URL + '/deleteCustomer/' + id);
+    } catch (err) {
+        console.log(err);
+    }
+}
